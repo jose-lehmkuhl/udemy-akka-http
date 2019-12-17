da278cb4985dc1f8860e35a49d27bd08c65f79bf
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-12-17 15:54:47 -0300

    Marshalling JSON

---
+++ src/main/scala/part2_lowlevelserver/LowLevelRest.scala
@@ -0,0 +1,60 @@
+package part2_lowlevelserver
+
+import akka.actor.{Actor, ActorLogging, ActorSystem}
+import akka.stream.ActorMaterializer
+import spray.json._
+import spray.json.DefaultJsonProtocol
+
+case class Guitar(make: String, model: String)
+
+object GuitarDB {
+  case class CreateGuitar(guitar: Guitar)
+  case class GuitarCreated(id: Int)
+  case class FindGuitar(id: Int)
+  case object FindAllGuitars
+}
+class GuitarDB extends Actor with ActorLogging {
+  import GuitarDB._
+
+  var guitars: Map[Int, Guitar] = Map()
+  var currentGuitarId: Int = 0
+
+  override def receive: Receive = {
+    case FindAllGuitars =>
+      log.info("Searching for all Guitars")
+      sender() ! guitars.values.toList
+
+    case FindGuitar(id) =>
+      log.info(s"search guitar by id $id")
+      sender() ! guitars.get(id)
+
+    case CreateGuitar(guitar) =>
+      log.info(s"adding guitar $guitar with id $currentGuitarId")
+      guitars = guitars + (currentGuitarId -> guitar)
+      sender() ! GuitarCreated(currentGuitarId)
+      currentGuitarId += 1
+  }
+}
+
+trait GuitarStoreJsonProtocol extends DefaultJsonProtocol {
+  implicit val guitarFormat = jsonFormat2(Guitar)
+}
+
+object LowLevelRest extends App with GuitarStoreJsonProtocol {
+
+  implicit val system = ActorSystem("LowLevelRest")
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val simpleGuitar = Guitar("Fender", "Stratocaster")
+  println(simpleGuitar.toJson.prettyPrint)
+
+  val simpleGuitarJsonString =
+    """
+      |{
+      |  "make": "Fender",
+      |  "model": "Stratocaster"
+      |}
+      |""".stripMargin
+  println(simpleGuitarJsonString.parseJson.convertTo[Guitar])
+}
